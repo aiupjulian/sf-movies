8adffb9d64be630bd75e2c057362db556a5955d5
Julian Aiup <aiupjulian@gmail.com>
2019-09-10 16:41:23 -0300

    Add process.env port for heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const MongoClient = require('mongodb').MongoClient;
 
 const dbURL = process.env.DATABASE_URL || 'mongodb://localhost:27017';
